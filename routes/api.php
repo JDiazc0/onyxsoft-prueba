1ec4899f73a44fe660094f3baa71fe95838f5a11
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-12-27 21:10:43 -0500

    feat create endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,31 @@
 <?php
 
+use App\Http\Controllers\Api\AuthorController;
+use App\Http\Controllers\Api\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Author endpoints
+Route::group(['prefix' => 'author'], function () {
+    Route::get('/', [AuthorController::class, 'index']);
+    Route::get('/{author}', [AuthorController::class, 'show']);
+    Route::post('', [AuthorController::class, 'store']);
+    Route::put('/{author}', [AuthorController::class, 'update']);
+    Route::delete('/{author}', [AuthorController::class, 'destroy']);
+});
+
+// Book endpoints
+Route::group(['prefix' => 'book'], function () {
+    Route::get('/', [BookController::class, 'index']);
+    Route::get('/{book}', [BookController::class, 'show']);
+    Route::post('', [BookController::class, 'store']);
+    Route::put('/{book}', [BookController::class, 'update']);
+    Route::delete('/{book}', [BookController::class, 'destroy']);
+    // Rent book
+    Route::post('/{book}/rent', [BookController::class, 'rentBook']);
+    Route::post('/{book}/return', [BookController::class, 'returnBook']);
+    // Add Author to Book
+    Route::post('add-author/{book}/{author}', [BookController::class, 'addAuthor']);
+    Route::post('remove-author/{book}/{author}', [BookController::class, 'removeAuthor']);
 });
